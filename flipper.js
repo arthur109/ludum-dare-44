53f1c9d58670ff79f50e6e750d6c99c9c8a19486
Jack Liu <jackxuanliu@gmail.com>
2019-04-27 12:47:24 -0700

    add flipper.js

---
+++ flipper.js
@@ -0,0 +1,46 @@
+
+class Flipper {
+  constructor(front, back) {
+    this.front = front
+    this.back = back
+
+    this.rect_x = 0.0
+    this.rect_size = 0.0
+
+    this.flip_speed = 30.0
+  }
+
+  draw(g) {
+    let edge_x = this.rect_x - this.rect_size / 2.0
+
+    if (edge_x < 0) edge_x = 0
+    if (edge_x > width) edge_x = width
+
+    if (g) {
+      if (this.rect_size < width)
+        g.image(this.front, 0, 0)
+      if (this.rect_size >= 1.0)
+        g.image(this.back, edge_x, 0, this.rect_size, height, edge_x, 0, this.rect_size, height)
+    } else {
+      if (this.rect_size < width)
+        image(this.front, 0, 0)
+      if (this.rect_size >= 1.0)
+        image(this.back, edge_x, 0, this.rect_size, height, edge_x, 0, this.rect_size, height)
+    }
+  }
+
+  flip(x, y) {
+    this.rect_x = x
+
+    this.rect_size += this.flip_speed
+    if (this.rect_size > width) this.rect_size = width
+  }
+
+  unflip(x, y) {
+    this.rect_x = x
+
+    this.rect_size -= this.flip_speed
+    if (this.rect_size < 0) this.rect_size = 0
+  }
+}
+
